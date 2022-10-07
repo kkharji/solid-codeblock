72627b0ec8daefb79b3d5e891c33c472004a304b
kkharji <kkharji@protonmail.com>
2022-10-07 05:28:47 +0300

    init

---
+++ vite.prod.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite"
+import solidPlugin from "vite-plugin-solid"
+import tsconfigPaths from "vite-tsconfig-paths"
+import dts from "vite-plugin-dts"
+
+export default defineConfig({
+  plugins: [
+    solidPlugin({ babel: { presets: ["solid"] } }),
+    tsconfigPaths({ root: __dirname }), dts()
+  ],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "SolidJSCodeblock",
+      formats: ["es"],
+      fileName: "index"
+    },
+    rollupOptions: {
+      external: ["solid-js", "solid-js/store"]
+    }
+  },
+  optimizeDeps: {
+    exclude: ["examples"]
+  }
+})
